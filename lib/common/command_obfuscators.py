247a6f710ffc3de4cf744cb41f33998a8dabde4f
Andrew LeFevre <jalefevre@liberty.edu>
2018-08-09 18:13:22 -0400

    Refactored obfuscators for ease of use

--- lib/common/command_obfuscators.py
+++ lib/common/command_obfuscators.py
@@ -1,19 +1,21 @@
-from helpers import RandomGen
-from obfuscator import Obfuscator
-
 import re
 
+from helpers import RandomGen, choosePrefItem
+from obfuscator import Obfuscator
 
-class DeobfuscateStub(object):
+
+class Stub(object):
     """
     This class is in charge of generating a valid deobfuscation stub,
     taking care of properly escaping quotes in the user's input, 
     generating random variable names, and so on. 
     """
-    def __init__(self, name, sizeRating, speedRating, escapeQuotes, stub):
+    def __init__(self, name, binariesUsed, sizeRating, timeRating, escapeQuotes, stub):
         self.name = name
+        self.longName = self.name.replace(" ", "_").lower()
+        self.binariesUsed = binariesUsed
         self.sizeRating = sizeRating
-        self.speedRating = speedRating
+        self.timeRating = timeRating
         self.escapeQuotes = escapeQuotes
         self.stub = stub
         self.randGen = RandomGen()
@@ -30,121 +32,130 @@ class DeobfuscateStub(object):
             maxVarLen = 12
         
         genStub = self.stub
-        for var in re.findall("VAR\d+", self.stub):
+        for var in re.findall(r"VAR\d+", self.stub):
             genStub = self.stub.replace(var, self.randGen.randGenVar(minVarLen, maxVarLen))
 
-        return genStub.replace("CMD", userCmd)
+        genStub = genStub.replace("CMD", userCmd)
+
+        return "eval $({0})".format(genStub) 
 
 
 class CommandObfuscator(Obfuscator):
     """
     Base class for all command obfuscators. If an obfuscator requires
     a deobfuscation stub to execute, then it is a command obfuscator.
-    Those stubs are stored in the self.stubs attribute. Every command 
-    obfuscator should have multiple stubs, as many as possible. The
-    stubs should be numerous and diverse, consisting of a range of
-    size ratings and time ratings. If at all possible, at least one
-    perl and python stub should be included per command obfuscator.
+    
+    :param name: name of the CommandObfuscator
+    :param description: short description of what the CommandObfuscator does
+    :param sizeRating: rating from 1 to 5 of how much the CommandObfuscator 
+    increases the size of the overall payload
+    :param timeRating: rating from 1 to 5 of how much the CommandObfuscator 
+    increases the execution time of the overall payload
     """
-    def __init__(self, name, description, sizeRating, speedRating, sizePref, speedPref, userCmd):
-        super().__init__()
+    def __init__(self, name, description, sizeRating, timeRating):
+        super().__init__(name)
         
         self.name = name
         self.description = description
         self.sizeRating = sizeRating
-        self.speedRating = speedRating
-        self.sizePref = sizePref
-        self.speedPref = speedPref 
-        self.userCmd = userCmd
+        self.timeRating = timeRating
         self.stubs = []
         self.deobStub = None
+        self.originalCmd = ""
         self.payload = ""
 
-    def chooseStub(self):
-        maxSize = self.sizePref + 2
-        maxSpeed = self.speedPref + 2
+    def chooseStub(self, sizePref, timePref, binaryPref, usrStub):
+        if usrStub is not None:
+            for stub in self.stubs:
+                if stub.long_name == usrStub:
+                    self.deobStub = stub
+                    return
+        
+        self.deobStub = choosePrefItem(self.stubs, sizePref, timePref, binaryPref)
 
-        selectedStubs = [stub for stub in self.stubs if stub.sizeRating <= maxSize and stub.speedRating <= maxSpeed]
-        self.deobStub = self.randGen.randSelection(selectedStubs)
+    def addEval(self, obCmd):
+        return 
 
 
 class Reverse(CommandObfuscator):
-    def __init__(self, sizePref, speedPref, userCmd):
+    def __init__(self):
         super().__init__(
             name="Command Reverser",
             description="Reverses a command",
             sizeRating=1,
-            speedRating=1,
-            sizePref=sizePref,
-            speedPref=speedPref,
-            userCmd=userCmd)
+            timeRating=1
+        )
 
         self.stubs = [
-            DeobfuscateStub(
-                name="rev",
+            Stub(
+                name="bash rev",
+                binariesUsed="rev",
                 sizeRating=1,
-                speedRating=1,
+                timeRating=1,
                 escapeQuotes=True,
                 stub="""printf "CMD"|rev"""
             ),
-            DeobfuscateStub(
+            Stub(
                 name="perl scalar reverse",
+                binariesUsed="perl",
                 sizeRating=3,
-                speedRating=1,
+                timeRating=1,
                 escapeQuotes=True,
                 stub="""perl -lne 'print scalar reverse "CMD"'"""
             ),
-            DeobfuscateStub(
+            Stub(
                 name="python list reverse",
+                binariesUsed="python",
                 sizeRating=2,
-                speedRating=1,
+                timeRating=1,
                 escapeQuotes=True,
                 stub="""python -c 'print("CMD"[::-1])'"""
             )
         ]
 
-    def obfuscate(self):
-        self.chooseStub()
-
-        obCmd = self.userCmd[::-1]
-        self.payload = self.deobStub.genStub(self.sizePref, obCmd)
+    def obfuscate(self, sizePref, timePref, userCmd, binaryPref=None, usrStub=None):
+        self.originalCmd = userCmd
+        self.chooseStub(sizePref, timePref, binaryPref, usrStub)
+        
+        obCmd = self.originalCmd[::-1]
+        self.payload = self.deobStub.genStub(sizePref, obCmd)
         
         return self.payload
 
 
 class CaseSwap(CommandObfuscator):
-    def __init__(self, sizePref, speedPref, userCmd):
+    def __init__(self):
         super().__init__(
             name="Case Swapper",
             description="Flips the case of all alpha chars",
             sizeRating=2,
-            speedRating=1,
-            sizePref=sizePref,
-            speedPref=speedPref,
-            userCmd=userCmd
+            timeRating=1
         )
 
         self.stubs = [
-            DeobfuscateStub(
+            Stub(
                 name="bash case swap expansion",
+                binariesUsed="bash",
                 sizeRating=1,
-                speedRating=1,
+                timeRating=1,
                 escapeQuotes=True,
                 stub="""VAR1="CMD";${VAR1~~}"""
             ),
-            DeobfuscateStub(
+            Stub(
                 name="python swapcase",
+                binariesUsed="python",
                 sizeRating=2,
-                speedRating=1,
+                timeRating=1,
                 escapeQuotes=True,
                 stub="""python -c 'print("CMD".swapcase())'"""
             )
         ]
 
-    def obfuscate(self):
-        self.chooseStub()
+    def obfuscate(self, sizePref, timePref, userCmd, binaryPref=None, usrStub=None):
+        self.originalCmd = userCmd
+        self.chooseStub(sizePref, timePref, binaryPref, usrStub)
 
-        obCmd = self.userCmd.swapcase()
-        self.payload = self.deobStub.genStub(self.sizePref, obCmd)
+        obCmd = self.originalCmd.swapcase()
+        self.payload = self.deobStub.genStub(sizePref, obCmd)
 
         return self.payload
--- lib/common/token_obfuscators.py
+++ lib/common/token_obfuscators.py
@@ -1,8 +1,8 @@
-from helpers import RandomGen
-from obfuscator import Obfuscator
-
 from binascii import hexlify
 import string
+
+from helpers import RandomGen
+from obfuscator import Obfuscator
 
 
 class TokenObfuscator(Obfuscator):
@@ -10,41 +10,48 @@ class TokenObfuscator(Obfuscator):
     Base class for all token obfuscators. If the obfuscator is able to
     be deobfuscated and executed by bash at runtime, without a stub, 
     then it is a token obfuscator.
+
+    :param name: name of the Token Obfuscator
+    :param description: short description of what the TokenObfuscator does
+    :param sizeRating: rating from 1 to 5 of how much the TokenObfuscator 
+    increases the size of the overall payload
     """
-    def __init__(self, name, description, sizeRating, sizePref, userCmd):
-        super().__init__()
+    def __init__(self, name, description, sizeRating):
+        super().__init__(name)
 
         self.name = name
         self.description = description
         self.sizeRating = sizeRating
-        self.sizePref = sizePref
-        self.userCmd = userCmd
+        self.originalCmd = ""
         self.payload = ""
 
 
-class AnsiCString(TokenObfuscator):
-    def __init__(self, sizePref, userCmd):
+class AnsiCQuote(TokenObfuscator):
+    def __init__(self):
         super().__init__(
-            name="ANSI-C String",
+            name="ANSI-C Quote",
             description="ANSI-C quotes a string",
-            sizeRating=3,
-            sizePref=sizePref,
-            userCmd=userCmd)
+            sizeRating=3
+        )
     
-    def obfuscate(self):
+        self.SUBSTR_QUOTE_PROB = 33
+
+    def obfuscate(self, sizePref, userCmd):
+        self.originalCmd = userCmd
+        
         obCmd = "$'\\"
 
-        if self.sizePref == 1:
+        if sizePref == 1:
             maxChoice = 2
         else:
             maxChoice = 4
 
-        for char in self.userCmd:
+        for char in self.originalCmd:
             choice = self.randGen.randChoice(maxChoice)
 
             # If sizePref is 3, randomly ANSI-C quote substrings of the original 
             # userCmd and randomly add empty strings
-            if self.sizePref == 3 and len(obCmd) > 3 and self.randGen.probibility(33):
+            if sizePref == 3 and len(obCmd) > 3 and self.randGen.probibility(self.SUBSTR_QUOTE_PROB):
                 obCmd = obCmd[:-1] + "'" + "".join("''" for x in range(self.randGen.randGenNum(0, 5))) + "$'\\"
 
             if choice == 0:
