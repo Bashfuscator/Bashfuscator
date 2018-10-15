b50df0a51da1f7bfa50b333680b61bb36bfae1a2
Andrew LeFevre <jalefevre@liberty.edu>
2018-10-15 13:41:44 -0400

    Added module to make bashfuscator easier to use as a library

---
+++ bashfuscator/core/mutator_list.py
@@ -0,0 +1,32 @@
+from bashfuscator.lib.command_obfuscators import *
+from bashfuscator.lib.string_obfuscators import *
+from bashfuscator.lib.token_obfuscators import *
+from bashfuscator.lib.encoders import *
+from bashfuscator.lib.compressors import *
+
+
+commandObfuscators = [
+    CaseSwap(),
+    Reverse(),
+]
+
+stringObfuscators = [
+    FileGlob(),
+    FolderGlob(),
+    ForCode(),
+    HexHash(),
+]
+
+tokenObfuscators = [
+    AnsiCQuote(),
+]
+
+encoders = [
+    Base64(),
+    UrlEncode(),
+]
+
+compressors = [
+    Bzip2(),
+    Gzip(),
+]
