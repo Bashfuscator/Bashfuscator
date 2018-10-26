2c70d3a16b270c1cd690e6006604a92a55a879f1
Andrew LeFevre <jalefevre@liberty.edu>
2018-10-26 12:03:06 -0400

    Tweaked setup.py to work on RHEL-based distros correctlly, closes #7

--- setup.py
+++ setup.py
@@ -3,8 +3,27 @@ Get dependencies, install bashfuscator to PATH and enable auto-completion
 """
 from setuptools import setup, find_packages
 from setuptools.command.install import install
-from subprocess import Popen, PIPE
+from subprocess import call, Popen, PIPE
+from sys import exit
 import os
+
+
+def findArgcompletePath(command):
+    proc = Popen("which " + command, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
+    command1Path, __ = proc.communicate()
+
+    proc = Popen("which " + command + "3", stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
+    command2Path, __ = proc.communicate()
+
+    if command1Path:
+        finalCommandPath = command1Path
+    elif command2Path:
+        finalCommandPath = command2Path
+    else:
+        print("ERROR: python3-argcomplete is not installed, install it with your package manager to activate autocompletion")
+        exit(1)
+
+    return finalCommandPath[:-1]
 
 
 with open("README.md") as f:
@@ -32,21 +51,18 @@ if os.path.exists(bashrcFile):
     with open(bashrcFile, "r") as f:
         bashrcContent = f.read()
 
-    if '_python_argcomplete "bashfuscator"' not in bashrcContent:
+    if "bashfuscator" not in bashrcContent:
         print("\nRegistering bashfuscator in .bashrc for auto-completion ...")
-        proc = Popen("activate-global-python-argcomplete3 --user", shell=True)
-        proc.wait()
 
-        # this will write the configuration needed in .bashrc file
-        proc = Popen("register-python-argcomplete3 bashfuscator", stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
-        autoCompleteData, __ = proc.communicate()
+        proc = call(findArgcompletePath("activate-global-python-argcomplete") + " --user", shell=True)
 
+        argcompleteComment = "# Enables autocompletion of options for bashfuscator"
         with open(bashrcFile, "a") as f:
-            f.write("\n{0}\n".format(autoCompleteData))
+            f.write("\n\n{0}\n".format(argcompleteComment))
+            f.write('eval "$({0})"\n'.format(findArgcompletePath("register-python-argcomplete") + " bashfuscator"))
 
         print("Auto-completion has been installed.")
-        proc = Popen(". ~/.bashrc", shell=True)
-        proc.wait()
+        proc = call("source " + bashrcFile, executable="/bin/bash", shell=True)
 
 else:
     print("ERROR: could not find your .bashrc file, autocompletion will not enabled")
