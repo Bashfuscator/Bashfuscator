9695711b39b6e35a3b9df611e96ecb2f82aa24c1
Charity Barker <goooglechick@gmail.com>
2018-11-04 16:09:22 -0500

    Temp fix to wait for subprocess to finish.

--- bashfuscator/bin/time_measure.py
+++ bashfuscator/bin/time_measure.py
@@ -9,23 +9,21 @@ from subprocess import STDOUT, PIPE, Popen
 from bashfuscator.core.obfuscation_handler import ObfuscationHandler
 
 
-#Variables
+#Globals
 longObfName="token/special_char_only"
 repeat_cmd=":;\n"
+unobfTimeData=[]
+obfTimeData=[]
 
 #list of times to run the command.  These values were chosen to work well on a logrithmic scale:
 #iterations=["1", "2", "3", "4", "5", "10", "20", "30", "40", "50", "100", "200", "300", "400", "500", "1000", "2000", "3000", "4000", "5000", "10000", "20000", "30000", "40000", "50000"]
-iterations=["1", "2", "3", "4", "5", "10", "20", "30", "40", "50", "100", "200", "300", "400", "500"]
-
-unobfTimeData=[]
-obfTimeData=[]
-timeDelta=[]
+iterations=["1", "2", "3", "4", "5", "10", "20", "30", "40", "50", "100", "200", "300", "400", "500", "1000", "2000", "3000", "4000", "5000", "10000", "20000", "30000"]
 
 #Functions
 def timeRun(payload): #Returns double: time it took to run payload in bash
+	
 	mysetup='''from subprocess import STDOUT, PIPE, Popen
 from __main__ import payload, runProcess'''
-
 	snippet="runProcess(payload)"
 	t=timeit.timeit(setup=mysetup, stmt=snippet, number=1)
 	print(t)
@@ -33,19 +31,11 @@ from __main__ import payload, runProcess'''
 
 def runProcess(payload):
 	proc = Popen(payload, executable="/bin/bash", stdout=PIPE, stderr=STDOUT, shell=True, universal_newlines=True)
-	__, __ = proc.communicate()
-	return
+	
+	while proc.poll() is None:
+		time.sleep(0)
 
-def  processData(runTimeList):	#Returns list: "timeDelta" values for plotting
-	#Fill timeDelta list
-	#timeDelta is one element shorter than the runTime list, it's the time increase between runs, first value of timeDelta is always 0.
-	timeDeltaList=[]
-	for i in range(len(runTimeList)):
-		if i==0:
-			timeDeltaList.append(0)
-		else:
-			timeDeltaList.append(runTimeList[i]-runTimeList[i-1])
-	return timeDeltaList
+	return
 
 def plotAllTheThings(xList, yList, Title):
 	#Still a work in progress: I don't have the right numbers or processed lists yet.
@@ -60,7 +50,7 @@ def plotAllTheThings(xList, yList, Title):
 	}, auto_open=True)
 
 
-#Try catch
+#Add try catch
 for i in iterations:        #Yo Dawg, I heard you liked iterations.  So I iterated over your iterations.
 	j=0 #Index counter for list maniputlation
 	i=int(i)
@@ -80,6 +70,7 @@ for i in iterations:        #Yo Dawg, I heard you liked iterations.  So I iterat
 	##$n/$len_cmd"*1000"
 	obfTime=timeRun(obfCommand)
 	print(obfTime)
-	obfTimeData.append((obfTime-unobfTimeData[-1])/(i*len(repeat_cmd)*1000))	#Append to list
+	#obfTimeData.append((obfTime-unobfTimeData[-1])/(i*len(repeat_cmd)*1000))	#Append to list
+	obfTimeData.append(obfTime)
 
 plotAllTheThings(iterations, obfTimeData, longObfName)
